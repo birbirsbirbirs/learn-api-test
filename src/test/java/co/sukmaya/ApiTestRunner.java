6333f7d91aa3a763ec130632007a14bc2156e0ee
birbirs <birbirsbirbirs@gmail.com>
2024-09-29 09:38:49 -0500

    cucumber report added

--- src/test/java/co/sukmaya/ApiTestRunner.java
+++ src/test/java/co/sukmaya/ApiTestRunner.java
@@ -2,8 +2,17 @@ package co.sukmaya;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import static org.junit.jupiter.api.Assertions.*;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ApiTestRunner {
@@ -12,6 +21,18 @@ public class ApiTestRunner {
         Results results = Runner.path("classpath:co/sukmaya/api") //.tags("@api")
                 .outputCucumberJson(true)
                 .parallel(20);
+        generateReport(results.getReportDir());
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"),
+                "learn-api-test");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
+
 }
